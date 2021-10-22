96b7626cf56be2fc11c12309ba4529c6d3effa35
Basil El-Hindi <basilelhindi@gmail.com>
2021-10-22 16:35:11 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    public GUISkin layout;
+
+    GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+    }
+
+    public static void Score(string wallID)
+    {
+        if (wallID == "RightWall")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 60 - 200, 20, 100, 100), "" + PlayerScore1);
+        GUI.Label(new Rect(Screen.width / 2 - 60 + 260, 20, 100, 100), "" + PlayerScore2);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
+        {
+            PlayerScore1 = 0;
+            PlayerScore2 = 0;
+            ball.SendMessage("RestartGame", .5f, SendMessageOptions.RequireReceiver); 
+        }
+
+        if (PlayerScore1 == 5)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 60, 200, 2000, 1000), "PLAYER ONE WINS");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+        else if (PlayerScore2 == 5)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 60, 200, 2000, 1000), "PLAYER TWO WINS");
+            ball.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
